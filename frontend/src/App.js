d0b7eff8fbe7895e1995a99c9f635991b3d24183
khalid <khalid.ibr212@gmail.com>
2023-09-30 21:23:08 +0300

    routs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/HomePage/HomePage';
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Routes>
+        <Route path="*" element={"Page Not Found"} />
+        <Route exact path="/" element={  <Home />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
