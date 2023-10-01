5a0a48e921552c7f844733ac7aa3ec9748f13017
Mujeer Ahmed <mujeerahmedx@gmail.com>
2023-10-01 15:42:23 +0530

    Commit

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import json
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for the entire app
 
@@ -11,6 +12,13 @@ def hello():
 def records():
     ls = [{'timestamp': '2023-10-01 07:22:00', 'ping': 3000.34, 'response': 200},{'timestamp': '2023-10-01 07:23:00', 'ping': 4000.34, 'response': 200},{'timestamp': '2023-10-01 07:24:00', 'ping': 1000.34, 'response': 200}]
     return ls
+
+@app.route("/data")
+def data():
+    f = open('data.json')
+    data = json.load(f)
+    return data
+
 
 if __name__ == "__main__":
     app.run(debug=True)
