e14b5b42ca0585e3bc10bc44bed36757e94c2cc1
Mujeer Ahmed <mujeerahmedx@gmail.com>
2023-10-01 12:57:11 +0530

    Added sample data

--- backend/server.py
+++ backend/server.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/records")
+def records():
+    ls = [{'timestamp': '2023-10-01 07:22:00', 'ping': 3000.34, 'response': 200},{'timestamp': '2023-10-01 07:23:00', 'ping': 4000.34, 'response': 200},{'timestamp': '2023-10-01 07:24:00', 'ping': 1000.34, 'response': 200}]
+    return ls
+
 if __name__ == "__main__":
     app.run(debug=True)
