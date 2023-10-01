bd8b44f89d02628507718356aa83549934a91379
khalid <khalid.ibr212@gmail.com>
2023-10-01 13:22:36 +0300

    CORS added

--- backend/server.py
+++ backend/server.py
@@ -11,6 +11,7 @@ def hello():
 @app.route("/records")
 def records():
     ls = [{'timestamp': '2023-10-01 07:22:00', 'ping': 3000.34, 'response': 200},{'timestamp': '2023-10-01 07:23:00', 'ping': 4000.34, 'response': 200},{'timestamp': '2023-10-01 07:24:00', 'ping': 1000.34, 'response': 200}]
+    # ls = []
     return ls
 
 @app.route("/data")
