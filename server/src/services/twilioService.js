6615946f3a6de25c26204d5f2084381b34ec39a8
khalid <khalid.ibr212@gmail.com>
2023-12-29 22:33:40 +0300

    twilio bug fix

--- server/src/services/twilioService.js
+++ server/src/services/twilioService.js
@@ -3,9 +3,7 @@ require("dotenv").config();
 const { Twilio } = require("twilio");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const accountSid = "AC448b63f1aa68c1c73a1f42e72ae8edb3";
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-// const authToken = "a4b2927e672a5d2eb294752a75b5fa9f";
 const twilioClient = new Twilio(accountSid, authToken);
 const twilioPhoneNumber = "+16507279180";
 const recipientPhoneNumber = "+251985216795";
