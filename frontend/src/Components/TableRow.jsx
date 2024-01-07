6309bbb84d26a92970e2e884eeb3d527b510b47d
khalid <khalid.ibr212@gmail.com>
2024-01-07 19:49:22 +0300

    bug fix

--- frontend/src/Components/TableRow.jsx
+++ frontend/src/Components/TableRow.jsx
@@ -3,18 +3,18 @@ import React from "react";
 function TableRow({ name, link, status, timeStamp }) {
   return (
     <>
-      <tr class="">
+      <tr className="">
         <th
           scope="row"
-          class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+          className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
         >
-          <div class="ps-3">
-            <div class="text-base font-semibold truncate">{name}</div>
+          <div className="ps-3">
+            <div className="text-base font-semibold truncate">{name}</div>
           </div>
         </th>
-        <td class="px-6 py-4">{link}</td>
-        <td class="px-6 py-4">
-          <div class="flex items-center">
+        <td className="px-6 py-4">{link}</td>
+        <td className="px-6 py-4">
+          <div className="flex items-center">
             <div
               className={`h-2.5 w-2.5 rounded-full me-2 ${
                 status == "200" ? "bg-green-500" : "bg-red-500"
@@ -23,7 +23,7 @@ function TableRow({ name, link, status, timeStamp }) {
             {status}
           </div>
         </td>
-        <td class="px-6 py-4 text-sm text-gray-500">{timeStamp}</td>
+        <td className="px-6 py-4 text-sm text-gray-500">{timeStamp}</td>
       </tr>
     </>
   );
