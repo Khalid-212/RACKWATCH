bfe63afbd8234d1a88183c500d194b8a501f0822
khalid <khalid.ibr212@gmail.com>
2024-01-08 12:44:23 +0300

    car button link fix

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -24,7 +24,7 @@ function Card({ status, name, link }) {
               </p>
             </div>
           </div>
-          <a className=" w-full @[320px]/card:w-fit " href="/s/rtm-survey_web">
+          <a className=" w-full @[320px]/card:w-fit " href="#">
             <button className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background border border-input hover:bg-accent hover:text-accent-foreground h-10 py-2 px-4 w-full @[320px]/card:w-fit hover:bg-gray-900 hover:border-gray-800">
               View
             </button>
